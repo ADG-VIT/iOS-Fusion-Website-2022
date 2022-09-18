a0a90fbea0d96c6f47db6a630a481d80f2770323
eshan1925 <eshan@getmedesign.com>
2022-09-18 12:55:28 +0530

    minor fix

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
-var top=document.getElementById("top");
-        top.onscroll = function() {myFunction()};
-        function myFunction() {
-          if (top.scrollY> 3150) {
-            document.getElementById("active").className ="timeline-inner-textbox";
-            document.getElementById("child1").className = "timeline-inner-textbox active";
-            document.getElementById("timeline-elipse").style.top = "260px";
-            if(top.scrollY > 3300) {
-            document.getElementById("child2").className ="timeline-inner-textbox active";
-            document.getElementById("child1").className = "timeline-inner-textbox";
-            document.getElementById("timeline-elipse").style.top = "470px";
-          }
-          if(top.scrollY > 3550) {
-            document.getElementById("child3").className ="timeline-inner-textbox active";
-            document.getElementById("child2").className = "timeline-inner-textbox";
-            document.getElementById("timeline-elipse").style.top = "650px";
-          }if(top.scrollY < 3550) {
-            document.getElementById("child2").className ="timeline-inner-textbox active";
-            document.getElementById("child3").className = "timeline-inner-textbox";
-            document.getElementById("timeline-elipse").style.top = "470px";
-          }
-          }
-          if(top.scrollY < 3300) {
-            document.getElementById("child1").className ="timeline-inner-textbox active";
-            document.getElementById("child2").className = "timeline-inner-textbox";
-            document.getElementById("timeline-elipse").style.top = "260px";
-          }if(top.scrollY < 3150) {
-            document.getElementById("child1").className = "timeline-inner-textbox";
-            document.getElementById("active").className ="timeline-inner-textbox active";
-            document.getElementById("timeline-elipse").style.top = "80px";
-            
-          }
-          
-        }
+var top = document.getElementById("top2");
+top.onscroll = function () {
+  myFunction();
+};
+function myFunction() {
+  if (top.scrollY > 3150) {
+    document.getElementById("active").className = "timeline-inner-textbox";
+    document.getElementById("child1").className =
+      "timeline-inner-textbox active";
+    document.getElementById("timeline-elipse").style.top = "260px";
+    if (top.scrollY > 3300) {
+      document.getElementById("child2").className =
+        "timeline-inner-textbox active";
+      document.getElementById("child1").className = "timeline-inner-textbox";
+      document.getElementById("timeline-elipse").style.top = "470px";
+    }
+    if (top.scrollY > 3550) {
+      document.getElementById("child3").className =
+        "timeline-inner-textbox active";
+      document.getElementById("child2").className = "timeline-inner-textbox";
+      document.getElementById("timeline-elipse").style.top = "650px";
+    }
+    if (top.scrollY < 3550) {
+      document.getElementById("child2").className =
+        "timeline-inner-textbox active";
+      document.getElementById("child3").className = "timeline-inner-textbox";
+      document.getElementById("timeline-elipse").style.top = "470px";
+    }
+  }
+  if (top.scrollY < 3300) {
+    document.getElementById("child1").className =
+      "timeline-inner-textbox active";
+    document.getElementById("child2").className = "timeline-inner-textbox";
+    document.getElementById("timeline-elipse").style.top = "260px";
+  }
+  if (top.scrollY < 3150) {
+    document.getElementById("child1").className = "timeline-inner-textbox";
+    document.getElementById("active").className =
+      "timeline-inner-textbox active";
+    document.getElementById("timeline-elipse").style.top = "80px";
+  }
+}
