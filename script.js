325b1834344dcc5e35d0a445a14deff9bbed039c
Udith Shyamsukha <udithshyamsukha@gmail.com>
2022-09-19 12:45:28 +0530

    Minor Bug fix

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const toggles = document.querySelectorAll('.faq-toggle');
+var top = document.getElementById('top');
+var responsive = window.matchMedia('(max-width: 520px)');
 
 toggles.forEach((toggle) => {
 	toggle.addEventListener('click', () => {
@@ -35,3 +37,90 @@ var x = setInterval(function () {
 		document.getElementById('time').innerHTML = 'EXPIRED';
 	}
 }, 1000);
+
+top.onscroll = function () {
+	myFunction();
+};
+function myFunction() {
+	if (top.scrollY > 3150) {
+		document.getElementById('active').className = 'timeline-inner-textbox';
+		document.getElementById('child1').className =
+			'timeline-inner-textbox active';
+		document.getElementById('timeline-elipse').style.top = '260px';
+		if (top.scrollY > 3300) {
+			document.getElementById('child2').className =
+				'timeline-inner-textbox active';
+			document.getElementById('child1').className = 'timeline-inner-textbox';
+			document.getElementById('timeline-elipse').style.top = '470px';
+		}
+		if (top.scrollY > 3550) {
+			document.getElementById('child3').className =
+				'timeline-inner-textbox active';
+			document.getElementById('child2').className = 'timeline-inner-textbox';
+			document.getElementById('timeline-elipse').style.top = '650px';
+		}
+		if (top.scrollY < 3550) {
+			document.getElementById('child2').className =
+				'timeline-inner-textbox active';
+			document.getElementById('child3').className = 'timeline-inner-textbox';
+			document.getElementById('timeline-elipse').style.top = '470px';
+		}
+	}
+	if (top.scrollY < 3300) {
+		document.getElementById('child1').className =
+			'timeline-inner-textbox active';
+		document.getElementById('child2').className = 'timeline-inner-textbox';
+		document.getElementById('timeline-elipse').style.top = '260px';
+	}
+	if (top.scrollY < 3150) {
+		document.getElementById('child1').className = 'timeline-inner-textbox';
+		document.getElementById('active').className =
+			'timeline-inner-textbox active';
+		document.getElementById('timeline-elipse').style.top = '80px';
+	}
+
+	if (responsive.matches) {
+		if (top.scrollY > 2900) {
+			document.getElementById('active').className = 'timeline-inner-textbox';
+			document.getElementById('child1').className =
+				'timeline-inner-textbox active';
+			document.getElementById('timeline-elipse').style.top = '200px';
+			if (top.scrollY > 3000) {
+				document.getElementById('child2').className =
+					'timeline-inner-textbox active';
+				document.getElementById('child1').className =
+					'timeline-inner-textbox';
+				document.getElementById('timeline-elipse').style.top = '320px';
+			}
+			if (top.scrollY > 3150) {
+				document.getElementById('child3').className =
+					'timeline-inner-textbox active';
+				document.getElementById('child2').className =
+					'timeline-inner-textbox';
+				document.getElementById('timeline-elipse').style.top = '420px';
+			}
+			if (top.scrollY < 3150) {
+				document.getElementById('child2').className =
+					'timeline-inner-textbox active';
+				document.getElementById('child3').className =
+					'timeline-inner-textbox';
+				document.getElementById('timeline-elipse').style.top = '320px';
+			}
+		}
+		if (top.scrollY < 3000) {
+			document.getElementById('child1').className =
+				'timeline-inner-textbox active';
+			document.getElementById('child2').className = 'timeline-inner-textbox';
+			document.getElementById('timeline-elipse').style.top = '200px';
+		}
+		if (top.scrollY < 2900) {
+			document.getElementById('child1').className = 'timeline-inner-textbox';
+			document.getElementById('active').className =
+				'timeline-inner-textbox active';
+			document.getElementById('timeline-elipse').style.top = '80px';
+		}
+	}
+}
+
+myFunction(responsive); // Call listener function at run time
+responsive.addListener(myFunction); // Attach listener function on state changes
